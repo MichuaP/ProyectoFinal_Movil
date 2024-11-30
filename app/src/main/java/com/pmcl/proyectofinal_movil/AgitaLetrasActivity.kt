075317fd83c9ecad80f9e0f194920180556fb4a0
NadiaAcostaM <acostamirandanadiaa@gmail.com>
2024-11-30 16:46:00 -0600

    Completar Palabra y sonidos
    - Se agregaron sonidos al de completar palabra y se puso botón en la parte de sensores para el sonido del nombre de los objetos

--- app/src/main/java/com/pmcl/proyectofinal_movil/AgitaLetrasActivity.kt
+++ app/src/main/java/com/pmcl/proyectofinal_movil/AgitaLetrasActivity.kt
@@ -9,7 +9,6 @@ import android.media.MediaPlayer
 import android.os.Bundle
 import android.os.Handler
 import android.widget.ImageButton
-import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -19,8 +18,8 @@ import androidx.core.view.WindowInsetsCompat
 class AgitaLetrasActivity : AppCompatActivity(), SensorEventListener {
 
     private lateinit var textViewLetra: TextView
-    private lateinit var imageViewObjeto1: ImageView
-    private lateinit var imageViewObjeto2: ImageView
+    private lateinit var imageButtonObjeto1: ImageButton
+    private lateinit var imageButtonObjeto2: ImageButton
     private lateinit var textViewObjeto1: TextView
     private lateinit var textViewObjeto2: TextView
 
@@ -44,61 +43,54 @@ class AgitaLetrasActivity : AppCompatActivity(), SensorEventListener {
     private var canStartMovement = false
 
     // Clase para almacenar las imágenes, nombres y los sonidos
-    data class LetterImages(val name1: String, val name2: String, val imageRes1: Int, val imageRes2: Int, val soundRes: Int)
+    data class LetterImages(val name1: String, val name2: String, val imageRes1: Int, val imageRes2: Int, val soundRes: Int, val soundRes1: Int, val soundRes2: Int)
 
     // Mapa de letras a imágenes, nombres y sonidos
     private val letterImages = mapOf(
-        'A' to LetterImages("Aguacate", "Axolote", R.drawable.aguacate, R.drawable.axolote, R.raw.a),
-        'B' to LetterImages("Ballena", "Balón", R.drawable.ballena, R.drawable.balon, R.raw.b),
-        'C' to LetterImages("Casa", "Conejo", R.drawable.casa, R.drawable.conejo, R.raw.c),
-        'D' to LetterImages("Dado", "Dinosaurio", R.drawable.dado, R.drawable.dinosaurio, R.raw.d),
-        'E' to LetterImages("Elefante", "Estrella", R.drawable.elefante, R.drawable.estrella, R.raw.e),
-        'F' to LetterImages("Flor", "Foca", R.drawable.flor, R.drawable.foca, R.raw.f),
-        'G' to LetterImages("Gato", "Guitarra", R.drawable.gato, R.drawable.guitarra, R.raw.g),
-        'H' to LetterImages("Helado", "Hipopotamo", R.drawable.helado, R.drawable.hipopotamo, R.raw.h),
-        'I' to LetterImages("Iguana", "Imán", R.drawable.iguana, R.drawable.iman, R.raw.i),
-        'J' to LetterImages("Jaguar", "Jitomate", R.drawable.jaguar, R.drawable.jitomate, R.raw.j),
-        'K' to LetterImages("Kiwi", "Koala", R.drawable.kiwi, R.drawable.koala, R.raw.k),
-        'L' to LetterImages("León", "Luna", R.drawable.leon, R.drawable.luna, R.raw.l),
-        'M' to LetterImages("Mantarralla", "Manzana", R.drawable.mantarralla, R.drawable.manzana, R.raw.m),
-        'N' to LetterImages("Nube", "Nutria", R.drawable.nube, R.drawable.nutria, R.raw.n),
-        'O' to LetterImages("Ojo", "Oso", R.drawable.ojo, R.drawable.oso, R.raw.o),
-        'P' to LetterImages("Pato", "Pizza", R.drawable.pato, R.drawable.pizza, R.raw.p),
-        'Q' to LetterImages("Queso", "Quetzal", R.drawable.queso, R.drawable.quetzal, R.raw.q),
-        'R' to LetterImages("Rana", "Reloj", R.drawable.rana, R.drawable.reloj, R.raw.r),
-        'S' to LetterImages("Serpiente", "Sombrero", R.drawable.serpiente, R.drawable.sombrero, R.raw.s),
-        'T' to LetterImages("Taco", "Toro", R.drawable.taco, R.drawable.toro, R.raw.t),
-        'U' to LetterImages("Unicornio", "Uvas", R.drawable.unicornio, R.drawable.uvas, R.raw.u),
-        'V' to LetterImages("Vaca", "Ventana", R.drawable.vaca, R.drawable.ventana, R.raw.v),
-        'W' to LetterImages("Waffle", "Wombat", R.drawable.waffle, R.drawable.wombat, R.raw.w),
-        'X' to LetterImages("Xilófono", "Xolo", R.drawable.xilofono, R.drawable.xolo, R.raw.x),
-        'Y' to LetterImages("Yak", "Yoyo", R.drawable.yak, R.drawable.yoyo, R.raw.y),
-        'Z' to LetterImages("Zebra", "Zapatos", R.drawable.zebra, R.drawable.zapatos, R.raw.z)
+        'A' to LetterImages("Aguacate", "Axolote", R.drawable.aguacate, R.drawable.axolote, R.raw.a, R.raw.aguacate, R.raw.axolote),
+        'B' to LetterImages("Ballena", "Balón", R.drawable.ballena, R.drawable.balon, R.raw.b, R.raw.ballena, R.raw.balon),
+        'C' to LetterImages("Casa", "Conejo", R.drawable.casa, R.drawable.conejo, R.raw.c, R.raw.casa, R.raw.conejo),
+        'D' to LetterImages("Dado", "Dinosaurio", R.drawable.dado, R.drawable.dinosaurio, R.raw.d, R.raw.dado, R.raw.dinosaurio),
+        'E' to LetterImages("Elefante", "Estrella", R.drawable.elefante, R.drawable.estrella, R.raw.e, R.raw.elefante, R.raw.estrella),
+        'F' to LetterImages("Flor", "Foca", R.drawable.flor, R.drawable.foca, R.raw.f, R.raw.flor, R.raw.foca),
+        'G' to LetterImages("Gato", "Guitarra", R.drawable.gato, R.drawable.guitarra, R.raw.g, R.raw.gato, R.raw.guitarra),
+        'H' to LetterImages("Helado", "Hipopótamo", R.drawable.helado, R.drawable.hipopotamo, R.raw.h, R.raw.helado, R.raw.hipopotamo),
+        'I' to LetterImages("Iguana", "Imán", R.drawable.iguana, R.drawable.iman, R.raw.i, R.raw.iguana, R.raw.iman),
+        'J' to LetterImages("Jaguar", "Jitomate", R.drawable.jaguar, R.drawable.jitomate, R.raw.j, R.raw.jaguar, R.raw.jitomate),
+        'K' to LetterImages("Kiwi", "Koala", R.drawable.kiwi, R.drawable.koala, R.raw.k, R.raw.kiwi, R.raw.koala),
+        'L' to LetterImages("León", "Luna", R.drawable.leon, R.drawable.luna, R.raw.l, R.raw.leon, R.raw.luna),
+        'M' to LetterImages("Mantarraya", "Manzana", R.drawable.mantarralla, R.drawable.manzana, R.raw.m, R.raw.mantarralla, R.raw.manzana),
+        'N' to LetterImages("Nube", "Nutria", R.drawable.nube, R.drawable.nutria, R.raw.n, R.raw.nube, R.raw.nutria),
+        'O' to LetterImages("Ojo", "Oso", R.drawable.ojo, R.drawable.oso, R.raw.o, R.raw.ojo, R.raw.oso),
+        'P' to LetterImages("Pato", "Pizza", R.drawable.pato, R.drawable.pizza, R.raw.p, R.raw.pato, R.raw.pizza),
+        'Q' to LetterImages("Queso", "Quetzal", R.drawable.queso, R.drawable.quetzal, R.raw.q, R.raw.queso, R.raw.quetzal),
+        'R' to LetterImages("Rana", "Reloj", R.drawable.rana, R.drawable.reloj, R.raw.r, R.raw.rana, R.raw.reloj),
+        'S' to LetterImages("Serpiente", "Sombrero", R.drawable.serpiente, R.drawable.sombrero, R.raw.s, R.raw.serpiente, R.raw.sombrero),
+        'T' to LetterImages("Taco", "Toro", R.drawable.taco, R.drawable.toro, R.raw.t, R.raw.taco, R.raw.toro),
+        'U' to LetterImages("Unicornio", "Uvas", R.drawable.unicornio, R.drawable.uvas, R.raw.u, R.raw.unicornio, R.raw.uvas),
+        'V' to LetterImages("Vaca", "Ventana", R.drawable.vaca, R.drawable.ventana, R.raw.v, R.raw.vaca, R.raw.ventana),
+        'W' to LetterImages("Waffle", "Wombat", R.drawable.waffle, R.drawable.wombat, R.raw.w, R.raw.waffle, R.raw.wombat),
+        'X' to LetterImages("Xilófono", "Xolo", R.drawable.xilofono, R.drawable.xolo, R.raw.x, R.raw.xilofono, R.raw.xolo),
+        'Y' to LetterImages("Yak", "Yoyo", R.drawable.yak, R.drawable.yoyo, R.raw.y, R.raw.yak, R.raw.yoyo),
+        'Z' to LetterImages("Zebra", "Zapatos", R.drawable.zebra, R.drawable.zapatos, R.raw.z, R.raw.zebra, R.raw.zapatos)
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.agita_letras)
 
-        // Configuración de la interfaz
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        // Enlaza los elementos de la interfaz
+        textViewLetra = findViewById(R.id.textViewLetra)
+        imageButtonObjeto1 = findViewById(R.id.imageButtonObjeto1)
+        imageButtonObjeto2 = findViewById(R.id.imageButtonObjeto2)
+        textViewObjeto1 = findViewById(R.id.textViewObjeto1)
+        textViewObjeto2 = findViewById(R.id.textViewObjeto2)
 
         // Enlaza el botón de volver
         val btnVolver: ImageButton = findViewById(R.id.btnVolver)
         btnVolver.setOnClickListener {
             finish() // Cierra la actividad
         }
-
-        // Enlaza los elementos de la interfaz
-        textViewLetra = findViewById(R.id.textViewLetra)
-        imageViewObjeto1 = findViewById(R.id.imageViewObjeto1)
-        imageViewObjeto2 = findViewById(R.id.imageViewObjeto2)
-        textViewObjeto1 = findViewById(R.id.textViewObjeto1)
-        textViewObjeto2 = findViewById(R.id.textViewObjeto2)
 
         // Inicializamos el SensorManager y el acelerómetro
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
@@ -109,6 +101,21 @@ class AgitaLetrasActivity : AppCompatActivity(), SensorEventListener {
             canStartMovement = true // Permite empezar el movimiento después de 3 segundos
             updateLetterAndImages() // Muestra la primera letra y su imagen
         }, 3000) // 3000 milisegundos = 3 segundos
+
+        // Configura los listeners para los ImageButton
+        imageButtonObjeto1.setOnClickListener {
+            val currentLetter = alphabet[currentLetterIndex]
+            val letterData = letterImages[currentLetter]
+            val mediaPlayer = MediaPlayer.create(this, letterData?.soundRes1 ?: 0)
+            mediaPlayer.start()
+        }
+
+        imageButtonObjeto2.setOnClickListener {
+            val currentLetter = alphabet[currentLetterIndex]
+            val letterData = letterImages[currentLetter]
+            val mediaPlayer = MediaPlayer.create(this, letterData?.soundRes2 ?: 0)
+            mediaPlayer.start()
+        }
     }
 
     override fun onResume() {
@@ -161,8 +168,8 @@ class AgitaLetrasActivity : AppCompatActivity(), SensorEventListener {
 
         runOnUiThread {
             textViewLetra.text = currentLetter.toString()
-            imageViewObjeto1.setImageResource(letterData?.imageRes1 ?: 0) // Sin valores predeterminados
-            imageViewObjeto2.setImageResource(letterData?.imageRes2 ?: 0) // Sin valores predeterminados
+            imageButtonObjeto1.setImageResource(letterData?.imageRes1 ?: 0) // Sin valores predeterminados
+            imageButtonObjeto2.setImageResource(letterData?.imageRes2 ?: 0) // Sin valores predeterminados
             textViewObjeto1.text = letterData?.name1 ?: "" // Sin valores predeterminados
             textViewObjeto2.text = letterData?.name2 ?: "" // Sin valores predeterminados
 
--- app/src/main/java/com/pmcl/proyectofinal_movil/Completar.kt
+++ app/src/main/java/com/pmcl/proyectofinal_movil/Completar.kt
@@ -11,6 +11,7 @@ import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.math.roundToInt
 
 class Completar(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
@@ -39,6 +40,15 @@ class Completar(context: Context, attrs: AttributeSet) : View(context, attrs) {
         textAlign = Paint.Align.CENTER
         typeface = Typeface.DEFAULT_BOLD
     }
+
+    // Texto de las letras de opciòn
+    private val pTextoOpciones = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        color = Color.WHITE  // La letra siempre será blanca
+        textSize = 150f  // Aumentar el tamaño de las letras (ajustar este valor para hacerlo más grande)
+        textAlign = Paint.Align.CENTER
+        typeface = Typeface.DEFAULT_BOLD
+    }
+
     // Definir un nuevo Paint para la letra correcta (color rojo)
     private val pTextoCorrecto = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         color = Color.RED  // Color rojo
@@ -47,15 +57,35 @@ class Completar(context: Context, attrs: AttributeSet) : View(context, attrs) {
         typeface = Typeface.DEFAULT_BOLD
     }
 
+    private val escala = resources.displayMetrics.density
+
     // Espacios y márgenes
     private val espacioEntreRectangulos = 20f
     private val altoRectangulo = 150f
     private val anchoRectangulo = 120f
+    private val altoRectanguloOpciones = 250f  // Aumentar la altura de las opciones de letras
+    private val anchoRectanguloOpciones = 250f  // Aumentar el ancho de las opciones de letras
+
 
     // MediaPlayer para los sonidos
     private var mediaPlayerCorrecto: MediaPlayer? = null
     private var mediaPlayerIncorrecto: MediaPlayer? = null
     private var mediaPlayerFinish: MediaPlayer? = null
+
+    // Colores para las opciones de letras
+    private val coloresOpciones = listOf(
+        Color.parseColor("#FDED32"),  // Amarillo
+        Color.parseColor("#FD4343"),  // Rojo
+        Color.parseColor("#70d6ff")   // Azul pastel
+    )
+
+    private fun darkenColor(color: Int): Int {
+        val factor = 0.8f
+        val r = (Color.red(color) * factor).roundToInt()
+        val g = (Color.green(color) * factor).roundToInt()
+        val b = (Color.blue(color) * factor).roundToInt()
+        return Color.rgb(r, g, b)
+    }
 
     // Mapa de palabras a imágenes
     private val imagenesPalabras = mapOf(
@@ -101,6 +131,50 @@ class Completar(context: Context, attrs: AttributeSet) : View(context, attrs) {
         "ZAPATOS" to R.drawable.zapatos,
     )
 
+    // Mapa de palabras a sonidos
+    private val sonidosPalabras = mapOf(
+        "AXOLOTE" to R.raw.axolote,
+        "BALLENA" to R.raw.ballena,
+        "BALÓN" to R.raw.balon,
+        "CASA" to R.raw.casa,
+        "CONEJO" to R.raw.conejo,
+        "DADO" to R.raw.dado,
+        "FLOR" to R.raw.flor,
+        "FOCA" to R.raw.foca,
+        "GATO" to R.raw.gato,
+        "HELADO" to R.raw.helado,
+        "IGUANA" to R.raw.iguana,
+        "IMÁN" to R.raw.iman,
+        "JAGUAR" to R.raw.jaguar,
+        "KIWI" to R.raw.kiwi,
+        "KOALA" to R.raw.koala,
+        "LEÓN" to R.raw.leon,
+        "LUNA" to R.raw.luna,
+        "MANZANA" to R.raw.manzana,
+        "NUBE" to R.raw.nube,
+        "NUTRIA" to R.raw.nutria,
+        "OJO" to R.raw.ojo,
+        "OSO" to R.raw.oso,
+        "PATO" to R.raw.pato,
+        "PIZZA" to R.raw.pizza,
+        "QUESO" to R.raw.queso,
+        "QUETZAL" to R.raw.quetzal,
+        "RANA" to R.raw.rana,
+        "RELOJ" to R.raw.reloj,
+        "TACO" to R.raw.taco,
+        "TORO" to R.raw.toro,
+        "UVAS" to R.raw.uvas,
+        "VACA" to R.raw.vaca,
+        "VENTANA" to R.raw.ventana,
+        "WAFFLE" to R.raw.waffle,
+        "WOMBAT" to R.raw.wombat,
+        "XOLO" to R.raw.xolo,
+        "YAK" to R.raw.yak,
+        "YOYO" to R.raw.yoyo,
+        "ZEBRA" to R.raw.zebra,
+        "ZAPATOS" to R.raw.zapatos
+    )
+
     // Lista de todas las palabras posibles
     private val palabras = listOf(
         "Axolote", "Ballena", "Balón", "Casa", "Conejo", "Dado",
@@ -132,10 +206,16 @@ class Completar(context: Context, attrs: AttributeSet) : View(context, attrs) {
     }
 
     // Método para cambiar la palabra, la imagen y las letras
-    // Método para cambiar la palabra, la imagen y las letras
     private fun cambiarPalabraYImagen() {
+        // Comprobar si ya se han completado todas las palabras
+        if (palabrasUsadas.size == palabras.size) {
+            // Si todas las palabras se han usado, mostramos la alerta de finalización
+            onCompleteGame()
+            return // No cambiamos de palabra
+        }
+
         // Obtener una nueva palabra aleatoria que no haya sido usada antes
-        palabraActual = obtenerPalabraAleatoria()  // Aquí es donde obtienes la nueva palabra
+        palabraActual = obtenerPalabraAleatoria()  // Aquí es donde se obtiene la nueva palabra
         letraFaltante = palabraActual.random()  // Nueva letra faltante
         letrasPosibles = generarLetrasPosibles()  // Generar nuevas letras posibles
 
@@ -169,7 +249,7 @@ class Completar(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
     // Método para obtener una palabra aleatoria
     private fun obtenerPalabraAleatoria(): String {
-        // Filtramos las palabras que ya han sido usadas
+        // Filtrar las palabras que ya han sido usadas
         val palabrasDisponibles = palabras.filter { !palabrasUsadas.contains(it.toUpperCase()) }
 
         if (palabrasDisponibles.isEmpty()) {
@@ -226,16 +306,32 @@ class Completar(context: Context, attrs: AttributeSet) : View(context, attrs) {
             xPos += anchoRectangulo + espacioEntreRectangulos
         }
 
-        // Centrar las opciones de letras
-        val totalAnchoOpciones = letrasPosibles.size * (anchoRectangulo + espacioEntreRectangulos) - espacioEntreRectangulos
-        var opcionX = (width - totalAnchoOpciones) / 2f  // Calculamos la posición X para centrar las opciones de letras
-
+        // Dibujar las opciones de letras con el color de fondo y la letra en blanco
+        val totalAnchoOpciones = letrasPosibles.size * (anchoRectanguloOpciones + espacioEntreRectangulos) - espacioEntreRectangulos
+        var opcionX = (width - totalAnchoOpciones) / 2f  // Centrar las opciones de letras horizontalmente
         var yPos = 350f  // Posición Y para las opciones de letras
-        for (letra in letrasPosibles) {
-            val rect = RectF(opcionX, yPos, opcionX + anchoRectangulo, yPos + altoRectangulo)
-            canvas.drawRoundRect(rect, 15f, 15f, pRectangulo)
-            canvas.drawText(letra.toString(), opcionX + anchoRectangulo / 2, yPos + altoRectangulo / 2 - (pTexto.descent() + pTexto.ascent()) / 2, pTexto)
-            opcionX += anchoRectangulo + espacioEntreRectangulos
+
+        for (i in letrasPosibles.indices) {
+            // Usamos los nuevos tamaños para los rectángulos
+            val rect = RectF(opcionX, yPos, opcionX + anchoRectanguloOpciones, yPos + altoRectanguloOpciones)
+
+            // Determinamos el color de fondo
+            val colorFondo = if (letraSeleccionadaCorrecta == letrasPosibles[i]) {
+                // Si la letra seleccionada es la correcta, usamos el color oscurecido
+                darkenColor(coloresOpciones[i % coloresOpciones.size])
+            } else {
+                // Si no es la correcta, usamos el color original
+                coloresOpciones[i % coloresOpciones.size]
+            }
+
+            // Dibujar el rectángulo con el color de fondo
+            canvas.drawRoundRect(rect, 15f * escala, 15f * escala, Paint().apply { color = colorFondo })
+
+            // Dibujar la letra en blanco (con el tamaño grande)
+            canvas.drawText(letrasPosibles[i].toString(), opcionX + anchoRectanguloOpciones / 2, yPos + altoRectanguloOpciones / 2 - (pTextoOpciones.descent() + pTextoOpciones.ascent()) / 2, pTextoOpciones)
+
+            // Mover la posición X para la siguiente opción
+            opcionX += anchoRectanguloOpciones + espacioEntreRectangulos
         }
     }
 
@@ -247,15 +343,16 @@ class Completar(context: Context, attrs: AttributeSet) : View(context, attrs) {
                 val y = event.y
 
                 // Verificar si el toque fue en alguna de las opciones de letras
-                if (y in 350f..(350f + altoRectangulo)) {
-                    var opcionX = (width - (letrasPosibles.size * (anchoRectangulo + espacioEntreRectangulos) - espacioEntreRectangulos)) / 2f  // Calculamos el desplazamiento en X para centrar las letras
+                if (y in 350f..(350f + altoRectanguloOpciones)) {
+                    var opcionX = (width - (letrasPosibles.size * (anchoRectanguloOpciones + espacioEntreRectangulos) - espacioEntreRectangulos)) / 2f  // Calculamos el desplazamiento en X para centrar las letras
 
                     for (i in letrasPosibles.indices) {
-                        val xPos = opcionX + i * (anchoRectangulo + espacioEntreRectangulos)
+                        val xPos = opcionX + i * (anchoRectanguloOpciones + espacioEntreRectangulos)
 
                         // Verificar si el toque está dentro de uno de los rectángulos de las letras
-                        if (x in xPos..(xPos + anchoRectangulo)) {
+                        if (x in xPos..(xPos + anchoRectanguloOpciones)) {
                             val letraSeleccionada = letrasPosibles[i]
+                            // Si la letra seleccionada es correcta
                             if (letraSeleccionada == letraFaltante) {
                                 // Reproducir sonido correcto
                                 mediaPlayerCorrecto?.start()
@@ -270,19 +367,25 @@ class Completar(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
                                 invalidate()  // Redibujar la vista para reflejar la letra seleccionada
 
-                                // Retrasar el cambio de palabra y la actualización de la imagen
-                                Handler().postDelayed({
-                                    // Cambiar la palabra, imagen y letras
-                                    cambiarPalabraYImagen()
+                                // Reproducir el sonido del animal correspondiente a la palabra actual
+                                val sonidoAnimalResId = sonidosPalabras[palabraActual]
+                                sonidoAnimalResId?.let {
+                                    MediaPlayer.create(context, it).start()
+                                }
 
-                                    // Verificar si ya se completaron todas las palabras
-                                    val totalPalabras = palabras.size // Número total de palabras en el juego
-                                    if (palabrasUsadas.size == totalPalabras) {
-                                        if (!palabraActual.contains(letraFaltante)) {
-                                            onCompleteGame()  // Llamamos al método de completar el juego
-                                        }
-                                    }
-                                }, 1500)  // Esperar 1.5 segundos
+                                // Verificar si todas las palabras han sido completadas
+                                if (palabrasUsadas.size == palabras.size) {
+                                    // Si todas las palabras se han completado, mostrar la alerta
+                                    Handler().postDelayed({
+                                        onCompleteGame()
+                                    }, 1500)  // Esperamos 1.5 segundos antes de mostrar la alerta
+                                } else {
+                                    // Retrasar el cambio de palabra y la actualización de la imagen
+                                    Handler().postDelayed({
+                                        // Cambiar la palabra, imagen y letras
+                                        cambiarPalabraYImagen()
+                                    }, 1500)  // Esperar 1.5 segundos
+                                }
                             } else {
                                 // Reproducir sonido incorrecto
                                 mediaPlayerIncorrecto?.start()
@@ -300,21 +403,15 @@ class Completar(context: Context, attrs: AttributeSet) : View(context, attrs) {
     // Método para cuando el juego se completa
     private fun onCompleteGame() {
         mediaPlayerFinish?.start()  // Sonido de finalización
-        Toast.makeText(context, "¡Juego completado!", Toast.LENGTH_LONG).show()
 
-        // Mostrar un diálogo con la opción de reiniciar
+        // Mostrar un diálogo con la opción de reiniciar o salir
         AlertDialog.Builder(context)
             .setTitle("¡Felicidades!")
             .setMessage("¡Has completado el juego!")
             .setNegativeButton("Salir") { dialog, _ ->
-                (context as AppCompatActivity).finish()  // Salir de la actividad
+                val activity = context as? AppCompatActivity
+                activity?.finish()  // Salir de la actividad actual
             }
             .show()
-    }
-
-    // Método para resetear el juego
-    private fun resetearJuego() {
-        palabrasUsadas.clear()
-        cambiarPalabraYImagen()
     }
 }
